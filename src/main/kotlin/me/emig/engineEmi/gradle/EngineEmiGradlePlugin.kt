da2dfddb047bb5dd055442f68186b39399058f9d
Nicolai Emig <nicolai.emig@amseegym.de>
2020-03-01 10:32:06 +0100

    Added RuncompatibilityMode

--- src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
+++ src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
@@ -4,9 +4,11 @@ import com.soywiz.korge.gradle.KorgeGradlePlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.net.URI
+import java.net.URL
 
 
-val engineEmiVersion = "0.43"
+//val engineEmiVersion = "0.43"
+val engineEmiVersion = URL("https://emig.me/engineEmi/version.txt").readText()
 //val engineEmiVersion findProperty("engineEmiAndPluginVersion")
 val korgeBintrayUrl = "https://dl.bintray.com/korlibs/korlibs/"
 val engineEmiBintrayUrl = "https://dl.bintray.com/emign/engineEmi/"
@@ -41,6 +43,11 @@ open class EngineEmiGradlePlugin : Plugin<Project> {
         project.tasks.register("openLocal") {
             it.group = "engineEmi"
             it.dependsOn("runJvmFirstThread")
+        }
+
+        project.tasks.register("openLocalCompatibilityMode") {
+            it.group = "engineEmi"
+            it.dependsOn("runJvm")
         }
 
         project.tasks.register("openInBrowser") {
