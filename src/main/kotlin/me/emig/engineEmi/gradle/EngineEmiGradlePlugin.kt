87ec626282f0703f34c0e36bf117c75895c97f7b
Nicolai Emig <nicolai.emig@amseegym.de>
2020-03-10 09:05:33 +0100

    0.50 mit systemProp.org.gradle.internal.publish.checksums.insecure=true

--- src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
+++ src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
@@ -9,7 +9,7 @@ import java.net.URI
 //val engineEmiVersion = "0.43"
 //val engineEmiVersion = URL("https://emig.me/engineEmi/version.txt").readText()
 //val engineEmiVersion = "0.46-SNAPSHOT"
-val engineEmiVersion = "0.51"
+val engineEmiVersion = "0.50"
 
 val korgeBintrayUrl = "https://dl.bintray.com/korlibs/korlibs/"
 val engineEmiBintrayUrl = "https://dl.bintray.com/emign/engineEmi/"
