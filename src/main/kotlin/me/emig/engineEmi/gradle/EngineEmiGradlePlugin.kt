8cb123bee86e32fff0cfef854cec83f53c4d4f65
Nicolai Emig <nicolai.emig@amseegym.de>
2020-03-12 14:17:41 +0100

    0.56 try

--- src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
+++ src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
@@ -9,7 +9,7 @@ import java.net.URI
 //val engineEmiVersion = "0.43"
 //val engineEmiVersion = URL("https://emig.me/engineEmi/version.txt").readText()
 //val engineEmiVersion = "0.46-SNAPSHOT"
-val engineEmiVersion = "0.55"
+val engineEmiVersion = "0.56"
 
 val korgeBintrayUrl = "https://dl.bintray.com/korlibs/korlibs/"
 val engineEmiBintrayUrl = "https://dl.bintray.com/emign/engineEmi/"
@@ -19,7 +19,8 @@ open class EngineEmiGradlePlugin : Plugin<Project> {
 
         project.repositories.apply {
 
-        maven {
+            mavenLocal()
+            maven {
                 it.url = URI(korgeBintrayUrl)
                 it.content {
                     it.excludeGroup("Kotlin/Native")
