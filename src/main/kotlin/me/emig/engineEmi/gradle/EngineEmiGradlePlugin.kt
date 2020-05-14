127f4b00856831eeb0dfb7ecd7432338c90d9983
Nicolai Emig <nicolai.emig@amseegym.de>
2020-05-14 13:59:53 +0200

    0.76

--- src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
+++ src/main/kotlin/me/emig/engineEmi/gradle/EngineEmiGradlePlugin.kt
@@ -11,7 +11,7 @@ import java.net.URI
 val korgeBintrayUrl = "https://dl.bintray.com/korlibs/korlibs/"
 val engineEmiBintrayUrl = "https://dl.bintray.com/emign/engineEmi/"
 
-val engineEmiVersion  = "0.75"
+val engineEmiVersion  = "0.76"
 
 
 open class EngineEmiGradlePlugin : Plugin<Project> {
